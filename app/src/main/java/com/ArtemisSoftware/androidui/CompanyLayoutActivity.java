2b0db1cc524b7ed64ddfb3a90ca940560a42a958
gustavo <gustavo.maia@b2b.com.pt>
2020-04-07 17:55:33 +0100

    CompanyLayoutActivity Phone Portrait Layout part 1

---
+++ app/src/main/java/com/ArtemisSoftware/androidui/CompanyLayoutActivity.java
@@ -0,0 +1,14 @@
+package com.ArtemisSoftware.androidui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompanyLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_layout);
+    }
+}
