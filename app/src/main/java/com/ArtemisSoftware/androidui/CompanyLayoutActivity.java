7cc59ca71d63695cd02c9ab9c26de49591b7d6cc
gustavo <gustavo.maia@b2b.com.pt>
2020-04-08 18:06:26 +0100

    CompanyLayoutActivity Tablet Portrait Layout

--- app/src/main/java/com/ArtemisSoftware/androidui/CompanyLayoutActivity.java
+++ app/src/main/java/com/ArtemisSoftware/androidui/CompanyLayoutActivity.java
@@ -1,8 +1,13 @@
 package com.ArtemisSoftware.androidui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.drawable.RoundedBitmapDrawable;
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class CompanyLayoutActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class CompanyLayoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_layout);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.devslopesprofilelogo);
+        RoundedBitmapDrawable rounded = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+
+        rounded.setCornerRadius(15f);
+        ImageView logo = findViewById(R.id.logo);
+
+        logo.setImageDrawable(rounded);
     }
 }
