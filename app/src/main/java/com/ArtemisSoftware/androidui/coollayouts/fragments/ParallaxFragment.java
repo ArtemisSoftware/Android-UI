0c0f9f173f45ce7951c102ef3aa74dbb554075a9
gustavo <gustavo.maia@b2b.com.pt>
2020-05-14 15:28:04 +0100

    Class version

--- app/src/main/java/com/ArtemisSoftware/androidui/coollayouts/fragments/ParallaxFragment.java
+++ app/src/main/java/com/ArtemisSoftware/androidui/coollayouts/fragments/ParallaxFragment.java
@@ -1,111 +1,54 @@
 package com.ArtemisSoftware.androidui.coollayouts.fragments;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.ArtemisSoftware.androidui.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ParallaxFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ParallaxFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Arrays;
+import java.util.List;
+
+
 public class ParallaxFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    int color;
 
     public ParallaxFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ParallaxFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ParallaxFragment newInstance(String param1, String param2) {
-        ParallaxFragment fragment = new ParallaxFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @SuppressLint("ValidFragment")
+    public ParallaxFragment(int color) {
+        this.color = color;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_parallax, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_parallax, container, false);
-    }
+        final FrameLayout frameLayout = view.findViewById(R.id.dummyfrag_bg);
+        frameLayout.setBackgroundColor(color);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+        RecyclerView recyclerView = view.findViewById(R.id.dummyfrag_scrollableview);
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity().getBaseContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+/*
+        List<String> list = Arrays.asList(VersionModel.data);
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        SimpleRecyclerAdapter adapter = new SimpleRecyclerAdapter(list);
+        recyclerView.setAdapter(adapter);
+*/
+        return view;
     }
 }
---
+++ app/src/main/java/com/ArtemisSoftware/androidui/coollayouts/models/Version.java
@@ -0,0 +1,14 @@
+package com.ArtemisSoftware.androidui.coollayouts.models;
+
+public class Version {
+
+    public String name;
+
+    public static final String[] data = {"Cupcake", "Donut", "Eclair",
+            "Froyo", "Gingerbread", "Honeycomb",
+            "Icecream Sandwich", "Jelly Bean", "Kitkat", "Lollipop"};
+
+    public Version(String name){
+        this.name=name;
+    }
+}
