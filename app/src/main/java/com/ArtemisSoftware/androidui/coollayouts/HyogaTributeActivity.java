1b0539ecfa7cf334f172ff3a1419cbc2df020b19
gustavo <gustavo.maia@b2b.com.pt>
2020-05-14 12:43:29 +0100

    HyogaTributeActivity

--- app/src/main/java/com/ArtemisSoftware/androidui/coollayouts/HyogaTributeActivity.java
+++ app/src/main/java/com/ArtemisSoftware/androidui/coollayouts/HyogaTributeActivity.java
@@ -1,16 +1,108 @@
 package com.ArtemisSoftware.androidui.coollayouts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.ArtemisSoftware.androidui.R;
+import com.google.android.material.appbar.AppBarLayout;
 
-public class HyogaTributeActivity extends AppCompatActivity {
+public class HyogaTributeActivity extends AppCompatActivity
+        implements AppBarLayout.OnOffsetChangedListener{
+
+    private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR  = 0.9f;
+    private static final float PERCENTAGE_TO_HIDE_TITLE_DETAILS     = 0.3f;
+    private static final int ALPHA_ANIMATIONS_DURATION              = 200;
+
+    private boolean mIsTheTitleVisible          = false;
+    private boolean mIsTheTitleContainerVisible = true;
+
+    private LinearLayout mTitleContainer;
+    private TextView mTitle;
+    private AppBarLayout mAppBarLayout;
+    private Toolbar mToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hyoga_tribute);
+
+        bindActivity();
+
+        mAppBarLayout.addOnOffsetChangedListener(this);
+
+        //mToolbar.inflateMenu(R.menu.menu_main);
+        startAlphaAnimation(mTitle, 0, View.INVISIBLE);
+    }
+
+
+    private void bindActivity() {
+        mToolbar        = (Toolbar) findViewById(R.id.toolbar);
+        mTitle          = (TextView) findViewById(R.id.textview_title);
+        mTitleContainer = (LinearLayout) findViewById(R.id.linearlayout_title);
+        mAppBarLayout   = (AppBarLayout) findViewById(R.id.appbar);
+    }
+
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int offset) {
+
+        int maxScroll = appBarLayout.getTotalScrollRange();
+        float percentage = (float) Math.abs(offset) / (float) maxScroll;
+
+        handleAlphaOnTitle(percentage);
+        handleToolbarTitleVisibility(percentage);
+    }
+
+
+
+    private void handleToolbarTitleVisibility(float percentage) {
+        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
+
+            if(!mIsTheTitleVisible) {
+                startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                mIsTheTitleVisible = true;
+            }
+
+        } else {
+
+            if (mIsTheTitleVisible) {
+                startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
+                mIsTheTitleVisible = false;
+            }
+        }
+    }
+
+    private void handleAlphaOnTitle(float percentage) {
+        if (percentage >= PERCENTAGE_TO_HIDE_TITLE_DETAILS) {
+            if(mIsTheTitleContainerVisible) {
+                startAlphaAnimation(mTitleContainer, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
+                mIsTheTitleContainerVisible = false;
+            }
+
+        } else {
+
+            if (!mIsTheTitleContainerVisible) {
+                startAlphaAnimation(mTitleContainer, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                mIsTheTitleContainerVisible = true;
+            }
+        }
+    }
+
+
+    public static void startAlphaAnimation (View v, long duration, int visibility) {
+        AlphaAnimation alphaAnimation = (visibility == View.VISIBLE)
+                ? new AlphaAnimation(0f, 1f)
+                : new AlphaAnimation(1f, 0f);
+
+        alphaAnimation.setDuration(duration);
+        alphaAnimation.setFillAfter(true);
+        v.startAnimation(alphaAnimation);
     }
 }
