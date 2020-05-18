0a12d2f18b36cafd38d8f59b5a23b017d427da74
gustavo <gustavo.maia@b2b.com.pt>
2020-05-18 10:50:31 +0100

    Layout activity_material_up_concept

---
+++ app/src/main/java/com/ArtemisSoftware/androidui/coollayouts/MaterialUpConceptActivity.java
@@ -0,0 +1,16 @@
+package com.ArtemisSoftware.androidui.coollayouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ArtemisSoftware.androidui.R;
+
+public class MaterialUpConceptActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_up_concept);
+    }
+}
